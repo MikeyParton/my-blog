3bedea692fe1022a1326d6d08b45a50f8ea7b1ed
Mike Parton <mikeparton@live.com.au>
2016-08-20 23:30:40 -0300

    Added comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.build(comment_params)
+
+		if @comment.save
+			redirect_to article_path(@comment.article)
+		else
+
+		end
+	end
+
+	private
+
+		def comment_params
+			params.require(:comment).permit(:author, :body)
+		end
+
+end
