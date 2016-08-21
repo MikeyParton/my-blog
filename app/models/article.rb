2d0dfe79b4e234dfc571aaed25458b2bac22625b
Mike Parton <mikeparton@live.com.au>
2016-08-21 17:44:04 -0300

    add tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,22 @@ class Article < ApplicationRecord
 	validates :body, presence: true
 
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
+
+	def tag_list
+	  self.tags.collect do |tag|
+	    tag.name
+	  end.join(", ")
+	end
+
+	def tag_list=(tags_string)
+		tag_names = tags_string.downcase.split(",").map(&:strip).uniq
+
+		tags_to_assign = tag_names.collect do |tag_name|
+			Tag.find_or_create_by(name: tag_name)
+		end
+		self.tags = tags_to_assign
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 	
+	root "articles#index"
   	resources :articles do
   		resources :comments
 	end
-
-  root "articles#index"
-  get "/new", to:"articles#new"
-  post "/new", to:"articles#create"
+	resources :tags
+  
+	get "/new", to:"articles#new"
+	post "/new", to:"articles#create"
 
 end
